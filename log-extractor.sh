5ccfdd59a1b1e92a5c0f54628c733b397cdb47db
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2023-08-17 11:31:25 -0400

    Update log-extractor.sh

--- log-extractor.sh
+++ log-extractor.sh
@@ -12,3 +12,6 @@ find . -name "*request*log" -type f -exec cp {} extracted-logs \;
 cd extracted-logs
 
 rm *request-out*log
+
+#I always put all my projects into ~/workspace (developer cleanliness since a decade) - you probably need to change this line 
+python3 ~/workspace/requesttime-master/requestTime-3.py *
