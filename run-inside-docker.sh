603bfa157ded668523c4bd513d23fb857069fdcd
sam <skasimalla@gmail.com>
2024-03-16 06:47:13 -0400

    Updates

--- run-inside-docker.sh
+++ run-inside-docker.sh
@@ -33,6 +33,10 @@ python3 generate_report.py --output_filename $OUTPUT_FILE
 export UPLOAD_PATH="${UPLOAD_PATH}${FILE_PATH}/"
 echo ${UPLOAD_PATH}
 curl -u $USER:$TOKEN -i -T ./outputs/* "${UPLOAD_PATH}"
+#Uploading to backup location
+curl -u $USER:$TOKEN -i -T ./outputs/* "https://solengcustomersupport.jfrog.io/artifactory/customer-support-bundles-results/outputs/"
+curl -u $USER:$TOKEN -i -T ./outputs/* "https://solengcustomersupport.jfrog.io/artifactory/customer-support-bundles-results/${FILE_PATH}/"
+
 
 #Move the processed file to processed folder
 export FILE_PATH_MOVE="customer-support-bundles-processed/$FILE_PATH/"
